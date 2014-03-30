14c8f60b0379435fea86e96fdea82479491b91e0
Hasufel <Hasufel@users.noreply.github.com>
2014-03-30 19:33:04 +0200

    update for haxe 3.102

--- flash/utils/IDataInput.hx
+++ flash/utils/IDataInput.hx
@@ -10,8 +10,13 @@ interface IDataInput {
 	public function readBoolean ():Bool;
 	public function readByte ():Int;
 	public function readBytes (data:ByteArray, offset:Int = 0, length:Int = 0):Void;
+	#if (haxe_ver <= 3.101)
 	public function readDouble ():Float;
 	public function readFloat ():Float;
+	#else
+	public function readDouble (pos:Int):Float;
+	public function readFloat (pos:Int):Float;
+	#end
 	public function readInt ():Int;
 	public function readShort ():Int;
 	public function readUnsignedByte ():Int;
